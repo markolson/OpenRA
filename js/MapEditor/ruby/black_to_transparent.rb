4bf16ef5f3692edd1ceb35b540a43e52ca9d39a9
Jason Denney <jdenney@lgscout.com>
2011-12-28 13:20:23 -0500

    Modify black_to_transparent

--- js/MapEditor/ruby/black_to_transparent.rb
+++ js/MapEditor/ruby/black_to_transparent.rb
@@ -1,31 +1,17 @@
 require 'rubygems'
 require 'RMagick'
-require 'json'
 include Magick
 
-json_name = "temperate.json"
-dir_name = "TemperateBackup"
-output_dir_name = "Temperate"
-
-jsonFile = File.read(json_name)
-json = JSON.parse(jsonFile)
-
-not_visible = {}
-json.each {|k,v|
-  if v["visibleChunks"].include?(0)
-    not_visible[v["path"]+".png"] = v
-  end
-}
+dir_name = "source/"
+output_dir_name = "Actors"
 
 Dir.entries(dir_name).each do |filename|
   next if filename == "."
   next if filename == ".."
 
   img = ImageList.new(dir_name+ "/" + filename)
-  if not_visible.keys.include?(filename)
-    img = img.transparent("#000000", Magick::TransparentOpacity)
-    puts filename
-  end
+  img = img.transparent("#000000", Magick::TransparentOpacity)
+  puts filename
   img.write(output_dir_name + "/" + filename)
 end
 
